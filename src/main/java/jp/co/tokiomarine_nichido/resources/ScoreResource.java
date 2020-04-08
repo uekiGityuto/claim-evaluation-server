42970c08ecc8be504a297456b35f72dd3d50b462
Shingo Nakayama <e33857@jp.ibm.com>
2020-04-08 14:43:39 +0900

    JAX-RSでjson⇔java objectのシリアライズ・デシリアライズをする用に変更。
    返し値の型をmapからオブジェクトに変更したので、別途画面側の対応が必要。

--- src/main/java/jp/co/tokiomarine_nichido/resources/ScoreResource.java
+++ src/main/java/jp/co/tokiomarine_nichido/resources/ScoreResource.java
@@ -1,8 +1,6 @@
 package jp.co.tokiomarine_nichido.resources;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -11,13 +9,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 
 import jp.co.tokiomarine_nichido.models.Comment;
 import jp.co.tokiomarine_nichido.models.Feedback;
@@ -35,39 +27,28 @@ import jp.co.tokiomarine_nichido.services.ScoreService;
 public class ScoreResource {
 	@Inject
 	private ScoreService ss;
-	private Gson gson = new Gson();
-	private ObjectMapper om = new ObjectMapper(); // gsonからjson変換時、timestampのmillisecondが消失される現象対応用
+	// TODO: 【李】【中山】millisend問題は別途調査
+	//private ObjectMapper om = new ObjectMapper(); // gsonからjson変換時、timestampのmillisecondが消失される現象対応用
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getScoreList() throws JsonProcessingException {
-		Map<String, List<Score>> map = new HashMap<String, List<Score>>();
-		List<Score> scoreList = ss.getScoreList();
-		map.put("scoreList", scoreList);
-		return om.writeValueAsString(map);
+	public List<Score> getScoreList() {
+		return ss.getScoreList();
 	}
 
 	@GET
 	@Path("/{claimId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getScore(@PathParam("claimId") String claimId) throws JsonProcessingException {
-		Map<String, Score> map = new HashMap<String, Score>();
-		Score score = ss.getScore(claimId);
-		map.put("score", score);
-		return om.writeValueAsString(map);
+	public Score getScore(@PathParam("claimId") String claimId) {
+		return ss.getScore(claimId);
 	}
 
 	@POST
 	@Path("/{claimId}/feedback")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String updateFeedbackIsCorrect(@QueryParam("feedback") String json)
-			throws JsonMappingException, JsonProcessingException {
-		Feedback feedback = gson.fromJson(json, Feedback.class);
-		Map<String, Boolean> map = new HashMap<String, Boolean>();
-		Boolean result = ss.updateFeedbackIsCorrect(feedback);
-		map.put("update", result);
-		return om.writeValueAsString(map);
+	public boolean updateFeedbackIsCorrect(Feedback feedback) {
+		return ss.updateFeedbackIsCorrect(feedback);
 	}
 
 	// TODO: 【李】POST /{claimId}/Feedbackと統合する。
@@ -86,17 +67,7 @@ public class ScoreResource {
 	@Path("/{claimId}/comment")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public String updateComment(@QueryParam("comment") String json)
-			throws JsonMappingException, JsonProcessingException {
-		Comment comment = om.readValue(json, Comment.class);
-		Map<String, Object> map = new HashMap<String, Object>();
-		Boolean result = ss.updateComment(comment);
-		if (result) {
-			map.put("update", comment);
-		} else {
-			map.put("update", false);
-		}
-
-		return om.writeValueAsString(map);
+	public boolean updateComment(Comment comment) {
+		return ss.updateComment(comment);
 	}
 }
