5a55a5f895e2114646b34bf98fa569eba015782d
Shingo Nakayama <e33857@jp.ibm.com>
2020-04-10 17:25:07 +0900

    Observableの実装を一旦削除（必要に応じて復活）

--- src/main/java/jp/co/tokiomarine_nichido/services/ClientService.java
+++ src/main/java/jp/co/tokiomarine_nichido/services/ClientService.java
@@ -14,11 +14,7 @@ import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.client.JerseyClientBuilder;
-import org.glassfish.jersey.client.rx.rxjava.RxObservable;
-
 import jp.co.tokiomarine_nichido.util.PropertyManager;
-import rx.Observable;
 
 /**
  * 非同期通信Service
@@ -100,45 +96,5 @@ public class ClientService {
 	private String createQueryParameter(Map<String, String> params) {
 		return params.entrySet().stream().map(e -> String.join("=", e.getKey(), e.getValue()))
 				.reduce((s1, s2) -> String.join("&", s1, s2)).map(s -> "?" + s).orElse("");
-	}
-
-	public Observable<Response> rxCient(String method, String url, MultivaluedMap<String, Object> params) {
-
-		Observable<Response> ob = null;
-
-		if (method != null && url != null) {
-
-			MultivaluedMap<String, Object> headerMap = new MultivaluedHashMap<String, Object>();
-			// TODO: getAuthKey()
-//			SAMLAuth =
-			headerMap.add("Authorization", SAMLAuth);
-			headerMap.add("Content-type", MediaType.APPLICATION_JSON);
-
-			String lMethod = method.toLowerCase();
-			if (lMethod.equals("get")) {
-				StringBuilder urlParam = new StringBuilder();
-				params.forEach((key, values) -> {
-					if (urlParam.length() == 0) {
-						urlParam.append("?");
-					} else {
-						urlParam.append("&");
-					}
-					urlParam.append(key + "=" + String.valueOf(values.get(0)));
-				});
-				url += urlParam;
-				ob = RxObservable.from(new JerseyClientBuilder().build()).target(url).request().headers(headerMap).rx()
-						.get();
-			} else if (lMethod.equals("post")) {
-				Form form = new Form();
-				params.forEach((key, values) -> {
-					form.param(key, String.valueOf(values.get(0)));
-				});
-				Entity<Form> entity = Entity.entity(form, MediaType.APPLICATION_JSON_TYPE);
-				ob = RxObservable.from(new JerseyClientBuilder().build()).target(url).request().headers(headerMap).rx()
-						.post(entity);
-			}
-		}
-
-		return ob;
 	}
 }
