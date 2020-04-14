dfeb0348dbbb9c166be4f58a5baf04a230d7d46b
Shingo Nakayama <e33857@jp.ibm.com>
2020-04-14 14:00:32 +0900

    Modelを理想と思う形に修正（Scoreクラスのみ）。

--- src/main/java/jp/co/tokiomarine_nichido/models/Score.java
+++ src/main/java/jp/co/tokiomarine_nichido/models/Score.java
@@ -3,9 +3,6 @@ package jp.co.tokiomarine_nichido.models;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import jp.co.tokiomarine_nichido.util.DateUtil;
 
 /**
  * 推論結果Score
@@ -13,109 +10,12 @@ import jp.co.tokiomarine_nichido.util.DateUtil;
  * @modify SKK231099 李
  */
 public class Score {
+	public String fraudScoreId;
+	public Integer score;
+	public Timestamp createDate;
+	public String claimId;
 
-	private String fraudScoreId;
-	private Integer score;
-	private Timestamp createDate;
-	private String claimId;
-
-	private Claim claim;
-	private List<Reason> reasons = new ArrayList<>();
-	private Feedback feedback;
-
-	private DateUtil dateUtil;
-
-	public Score() {
-	}
-
-	public Score(String fraudScoreId, Integer score, Timestamp createDate, String claimId, Claim claim) {
-		this.fraudScoreId = fraudScoreId;
-		this.score = score;
-		this.createDate = createDate;
-		this.claimId = claimId;
-		this.claim = claim;
-		reasons = new ArrayList<Reason>();
-		feedback = new Feedback();
-	}
-
-	public Score(Map<String, Object> obj) {
-		String fraudScoreId = String.valueOf(obj.get("fraudScoreId"));
-		String scoreStr = String.valueOf(obj.get("score"));
-		String createDate = String.valueOf(obj.get("createDate"));
-		String claimId = String.valueOf(obj.get("claimId"));
-		@SuppressWarnings("unchecked")
-		Map<String, Object> claimMap = (Map<String, Object>) obj.get("claim");
-		Claim claim = new Claim(claimMap);
-		@SuppressWarnings("unchecked")
-		List<Map<String, Object>> reasonList = (List<Map<String, Object>>) obj.get("reasons");
-		List<Reason> reasons = new ArrayList<Reason>();
-		for (Map<String, Object> reasonMap : reasonList) {
-			Reason reason = new Reason(reasonMap);
-			reasons.add(reason);
-		}
-
-		this.fraudScoreId = fraudScoreId;
-		this.score = (int) Math.round(Double.valueOf(scoreStr));
-		dateUtil = new DateUtil();
-		this.createDate = dateUtil.toTimestamp(createDate);
-		this.claimId = claimId;
-		this.claim = claim;
-		this.reasons = reasons;
-	}
-
-	public String getFraudScoreId() {
-		return fraudScoreId;
-	}
-
-	public void setFraudScoreId(String fraudScoreId) {
-		this.fraudScoreId = fraudScoreId;
-	}
-
-	public Integer getScore() {
-		return score;
-	}
-
-	public void setScore(Integer score) {
-		this.score = score;
-	}
-
-	public Timestamp getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(Timestamp createDate) {
-		this.createDate = createDate;
-	}
-
-	public String getClaimId() {
-		return claimId;
-	}
-
-	public void setClaimId(String claimId) {
-		this.claimId = claimId;
-	}
-
-	public Claim getClaim() {
-		return claim;
-	}
-
-	public void setClaim(Claim claim) {
-		this.claim = claim;
-	}
-
-	public List<Reason> getReasons() {
-		return reasons;
-	}
-
-	public void setReasons(List<Reason> reasons) {
-		this.reasons = reasons;
-	}
-
-	public Feedback getFeedback() {
-		return feedback;
-	}
-
-	public void setFeedback(Feedback feedback) {
-		this.feedback = feedback;
-	}
+	public Claim claim;
+	public List<Reason> reasons = new ArrayList<>();
+	public Feedback feedback;
 }
--- src/main/java/jp/co/tokiomarine_nichido/services/ScoreService.java
+++ src/main/java/jp/co/tokiomarine_nichido/services/ScoreService.java
@@ -1,6 +1,5 @@
 package jp.co.tokiomarine_nichido.services;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,8 +9,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
-
-import com.google.gson.Gson;
 
 import jp.co.tokiomarine_nichido.models.Comment;
 import jp.co.tokiomarine_nichido.models.Feedback;
@@ -29,7 +26,6 @@ import jp.co.tokiomarine_nichido.util.PropertyManager;
 @ApplicationScoped
 public class ScoreService {
 	private String fraudScoreURL = "";
-	private Gson gson = new Gson();
 
 	@Inject
 	private PropertyManager pm;
@@ -61,7 +57,7 @@ public class ScoreService {
 		// 不正請求DBからCommentとFeedbackを結合
 		List<Comment> comments = cs.getComments(claimId);
 		for (Score score : scores) {
-			score.getClaim().setCommentList(comments);
+			score.claim.setCommentList(comments);
 		}
 
 		return scores;
@@ -82,41 +78,6 @@ public class ScoreService {
 		}
 
 		return scoreList;
-	}
-
-	public Score getScore(String claimId) {
-		Score score = null;
-		Map<String, String> errMsgList = new HashMap<String, String>();
-
-		Map<String, Object> param = new HashMap<String, Object>();
-		param.put("claimId", claimId);
-		Response rs = cService.client("get", fraudScoreURL, param);
-		if (rs != null) {
-			try {
-				String json = rs.readEntity(String.class);
-
-				// TODO: remove this code for json-server after set GNetAPI
-				// for json-server
-				@SuppressWarnings("unchecked")
-				List<Map<String, Object>> list = gson.fromJson(json, ArrayList.class);
-				score = new Score(list.get(0));
-
-//				score = gzon.fromJson(json, Score.class);
-
-				List<Comment> commentList = cs.getComments(score.getClaimId());
-				score.getClaim().setCommentList(commentList);
-				Feedback feedback = fs.getFeedback(score.getFraudScoreId());
-				score.setFeedback(feedback);
-
-			} catch (Exception e) {
-				errMsgList.put("status(" + rs.getStatus() + "): ", String.valueOf(rs.getStatusInfo()));
-			}
-		} else {
-			if (errMsgList.size() == 0) {
-				errMsgList.put("0", "Please Check method and url");
-			}
-		}
-		return score;
 	}
 
 	public Boolean updateFeedbackIsCorrect(Feedback feedback) {
